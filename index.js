abcb57d2341402ca26116f8efde462e1e39f13e0
Catherine Loesch <catherine.loesch@protonmail.com>
2021-06-03 19:11:16 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ] 
+
+
+function isWin(recordInstance){
+    return recordInstance.result === "W"
+}
+
+function superbowlWin(array){
+    const win = array.find(isWin);
+    if (!!win === true) {
+        return array.find(isWin).year
+    } else {
+        return undefined
+    }
+}
+
+
+
+console.log(superbowlWin(record));
